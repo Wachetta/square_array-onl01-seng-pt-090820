8ac78d9e634ea10f1acd64ff229883223b2202cc
Zachary Darneille <zakdarn@gmail.com>
2020-09-12 18:25:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,8 @@
 def square_array(numbers)
-num = []
+num = [] #create a place to store numbers
 numbers.each {|x| num << x ** 2}
+#call on numbers array with each. Have it take each element and exponetial it by 2.
+#Shovel each elment into the empty array
 num
+#have it return the array, avoid using the return
 end
